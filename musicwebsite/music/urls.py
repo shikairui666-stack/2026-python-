eee74380413ceb5a6a3bd81794b79573e74c1612
skr25 <1876752508@qq.com>
2026-08-25 09:14:35 +0800

    content_1

--- musicwebsite/music/urls.py
+++ musicwebsite/music/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
-import music.views as views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('blog/<int:id>', views.show_blog),
-    path('comment/<int:id>', views.comment)
+    path('', views.song_list, name='song_list'),                       # 主页 = 歌曲列表
+    path('song/<int:song_id>/comment/', views.add_comment, name='add_comment'),
+    path('song/<int:song_id>/', views.song_detail, name='song_detail'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('artist/', views.artist_list, name='artist_list'),
+    path('artist/<int:artist_id>/', views.artist_detail, name='artist_detail'),
 ]
